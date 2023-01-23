18674ef14d4458c1321f150f9e9e614ab44aa671
JuliansRecent <juliansrecent@gmail.com>
2023-01-22 19:55:27 -0800

    Module complete. Prompted user for a string, int, double, and boolean. Then printed them allto the console.

--- src/main/java/DifferentTypesOfInput.java
+++ src/main/java/DifferentTypesOfInput.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 public class DifferentTypesOfInput {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a string: ");
+        String string = scanner.nextLine();
+        System.out.println("Give an integer: ");
+        int integer = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give a double: ");
+        double duo = Double.valueOf(scanner.nextLine());
+        System.out.println("Give a boolean: ");
+        boolean bool = Boolean.valueOf(scanner.nextLine());
 
+        System.out.println("You gave the string " + string);
+        System.out.println("You gave the integer " + integer);
+        System.out.println("You gave the double " + duo);
+        System.out.println("You gave the boolean " + bool);
     }
 }
